489aec409f92351a9857aae510cd8265e7d69cb0
arkansoap <arkansoap@gmail.com>
2022-03-22 23:06:39 +0100

    update repo crea_pack testlin

--- CODE_R/aide_crea_pack/usePack.R
+++ CODE_R/aide_crea_pack/usePack.R
@@ -9,12 +9,26 @@ help("OlsDiag")
 help("graph_cor")
 help("covRect")
 help("covRect_resample")
+help("testlin")
 
 calcul_ind(data1$X, data1$Y1, N=nrow(data1))
 OlsDiag(data1$X, data1$Y1, N=nrow(data1))
 covRect(data1)[[1]]
 covRect_resample(data1, 100)[[1]]
 graph_cor(data1$X, data1$Y1, N=nrow(data1))
+
+# uniformiser notation x, y des datasets pr qu'ils soient ok avec toutes les fonctions
+################# A jarter qd j'aurai refais les datasets du package
+N = 400
+x <- runif(N,0,100)
+x <- sort(x)
+u <- rnorm(N,0,20)
+alpha=2
+y<--50+10*x^alpha+u
+mydata<-data.frame(cbind(x,y))
+####################################################"
+
+testlin(mydata)
 
 
 
